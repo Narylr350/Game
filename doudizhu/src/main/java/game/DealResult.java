51e088ec559c0c7f35f83381cea96b386e09ce60
Narylr <2378396898@qq.com>
2026-04-05 00:49:37 +0800

    更新权限

--- doudizhu/src/main/java/game/DealResult.java
+++ doudizhu/src/main/java/game/DealResult.java
@@ -7,16 +7,16 @@ public class DealResult {
     private final List<PlayerState> players;
     private final TreeSet<Integer> holeCards;
 
-    public DealResult(List<PlayerState> players, TreeSet<Integer> holeCards) {
+    protected DealResult(List<PlayerState> players, TreeSet<Integer> holeCards) {
         this.players = List.copyOf(players);
         this.holeCards = new TreeSet<>(holeCards);
     }
 
-    public List<PlayerState> getPlayers() {
+    protected List<PlayerState> getPlayers() {
         return players;
     }
 
-    public TreeSet<Integer> getHoleCards() {
+    protected TreeSet<Integer> getHoleCards() {
         return new TreeSet<>(holeCards);
     }
 }
